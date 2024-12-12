9b3b687ecf3d2d77474510184002ecb216af12e2
dl-dl <dl.toronto@outlook,com>
2024-12-12 14:04:25 -0400

    ADC raw read.

--- main/main.cpp
+++ main/main.cpp
@@ -289,7 +289,7 @@ extern "C" void app_main(void) {
 	                        &bleAdcFeedPublisherTaskHandle, CORE_BLE);
 
 	// // esp_pm_config_esp32_t pm_config = {
-	const esp_pm_config_t pmConfig = {
+	esp_pm_config_t pmConfig = {
 	    .max_freq_mhz       = 80,
 	    .min_freq_mhz       = 10,
 	    .light_sleep_enable = false,
