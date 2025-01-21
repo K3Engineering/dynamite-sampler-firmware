0f127d17d76d0afb54eb5b63bf95baf27d1ed0d4
larry <larry@k3engineering.com>
2025-01-21 15:32:34 -0500

    adding partition table class

--- flash_calibration.py
+++ flash_calibration.py
@@ -7,9 +7,49 @@ import struct
 
 import esptool
 
+# These are the defaults.
 PARTITION_TABLE_OFFSET = 0x8000
 MAX_PARTITION_LENGTH = 0xC00
-MAGIC_BYTES = b"\xAA\x50"
+
+
+class PartitionRow:
+    """Format and parsing of a partition table row on the ESP.
+    ESP tools has a class like this, but its really cumbersome, so this is a lite version.
+    """
+
+    MAGIC_BYTES = b"\xAA\x50"  # Each row starts with this magic number
+    STRUCT_FORMAT = b"<2sBBLL16sL"
+
+    def __init__(self, type, subtype, offset, size, name, flags):
+        self.type = type
+        self.subtype = subtype
+        self.offset = offset
+        self.size = size
+        self.name = name
+        self.flags = flags
+
+    def __str__(self):
+        # Since the format is fixed, the object string can be fixed width
+        self_str = (
+            "PartitionRow object: "
+            f"type: {self.type:#04x} "
+            f"subtype: {self.subtype:#04x} "
+            f"offset: {self.offset:#010x} "
+            f"size: {self.size:#010x} "
+            f"name: {self.name:16} "
+            f"flags: {self.flags:#010b} "
+        )
+        return self_str
+
+    @classmethod
+    def iter_from_raw_table(cls, data: bytes):
+        """Unpack a raw partition table, and yield each row"""
+        for vals in struct.iter_unpack(PartitionRow.STRUCT_FORMAT, data):
+            (magic, type, subtype, offset, size, name, flags) = vals
+            if magic == PartitionRow.MAGIC_BYTES:
+                name = name.strip(b"\x00").decode()
+                yield cls(type, subtype, offset, size, name, flags)
+
 
 ## Detect the port, connect to ESP
 ser_list = esptool.get_port_list([], [], [])
@@ -34,20 +74,13 @@ def flash_progress(progress, length):
 data = esp.read_flash(PARTITION_TABLE_OFFSET, MAX_PARTITION_LENGTH, flash_progress)
 print("got partition table")
 
-STRUCT_FORMAT = b"<2sBBLL16sL"
 
-partition_offset = {}
-for vals in struct.iter_unpack(STRUCT_FORMAT, data):
-    (magic, type, subtype, offset, size, name, flags) = vals
-    if magic != MAGIC_BYTES:
+flash_offset = None
+for row in PartitionRow.iter_from_raw_table(data):
+    print(row)
+    if row.name == "loadcell_calib":
+        flash_offset = row.offset
         break
-    name = name.strip(b"\x00").decode()
-    partition_offset[name] = offset
-    print(name, offset, hex(offset))
-
-print(partition_offset)
-## Find the correct offset
-flash_offset = partition_offset["loadcell_calib"]
 
 ## Generate the calibration data to flash
 calibration_data = struct.pack("<III", 11, 22, 33)
@@ -66,6 +99,7 @@ def progress_callback(percent):
     print(f"Wrote: {int(percent)}%")
 
 
+print(f"ESP flash write size {esp.FLASH_WRITE_SIZE}")
 esp.flash_begin(total_size, flash_offset)
 for i in range(0, total_size, esp.FLASH_WRITE_SIZE):
     block = calibration_data[i : i + esp.FLASH_WRITE_SIZE]
