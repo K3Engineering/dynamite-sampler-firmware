K3Engineering/dynamite-sampler-firmware

commit 85b4970565d684f8509e3c7fc8fc1855515834ba

Mock ADC (#31)
New class with the same interface as ADS131M0x.
To switch use `typedef ADS131M0x AdcClass;` or `typedef MockAdc
AdcClass;`
Also fixed #23

main/ADS131M0x.cpp
@@ -9,12 +9,27 @@
 // INFO Version for ADS131M02
 #endif
 
-/**
- * @brief Set SPI speed (call bevor "begin" to change default 2MHz)
- *
- * @param cspeed value in Hz
- */
-void ADS131M0x::setClockSpeed(uint32_t cspeed) { spiClockSpeed = cspeed; }
+static constexpr uint16_t crc16ccitt(const uint8_t *ptr, size_t count) {
+	static constexpr uint16_t CRC_INIT_VAL = 0xFFFF;
+	static constexpr uint16_t CRC_POLYNOM  = 0x1021;
+
+	uint16_t crc = CRC_INIT_VAL;
+	while (count > 0) {
+		--count;
+		static_assert(sizeof(*ptr << 8) >= sizeof(crc));
+		crc ^= *ptr << 8;
+		++ptr;
+		for (int i = 0; i < 8; ++i) {
+			if (crc & 0x8000) {
+				crc <<= 1;
+				crc ^= CRC_POLYNOM;
+			} else {
+				crc <<= 1;
+			}
+		}
+	}
+	return crc;
+}
 
 /**
  * @brief Write to ADC131M01 or ADC131M04 register
@@ -145,10 +160,6 @@ uint16_t ADS131M0x::readRegister(uint8_t address) {
  * @brief Write a value to the register, applying the mask to touch only the necessary bits.
  * It does not carry out the shift of bits (shift), it is necessary to pass the shifted value to the
  * correct position
- *
- * @param address
- * @param value
- * @param mask
  */
 void ADS131M0x::writeRegisterMasked(uint8_t address, uint16_t value, uint16_t mask) {
 	// Read the current content of the register
@@ -164,15 +175,24 @@ void ADS131M0x::writeRegisterMasked(uint8_t address, uint16_t value, uint16_t ma
 }
 
 /// @brief Hardware reset (reset low activ)
-/// @param reset_pin
-void ADS131M0x::reset(uint8_t reset_pin) {
-	pinMode(reset_pin, OUTPUT);
-	digitalWrite(reset_pin, HIGH);
+void ADS131M0x::reset() {
+	pinMode(resetPin, OUTPUT);
+	digitalWrite(resetPin, HIGH);
 	delay(100);
-	digitalWrite(reset_pin, LOW);
+	digitalWrite(resetPin, LOW);
 	delay(100);
-	digitalWrite(reset_pin, HIGH);
+	digitalWrite(resetPin, HIGH);
 	delay(1);
+	return;
+
+	// const gpio_num_t num = (gpio_num_t)resetPin;
+	// gpio_set_direction(num, GPIO_MODE_OUTPUT);
+	// gpio_set_level(num, 1);
+	// delay(100);
+	// gpio_set_level(num, 0);
+	// delay(100);
+	// gpio_set_level(num, 1);
+	// delay(1);
 }
 
 /**
@@ -184,31 +204,24 @@ uint16_t ADS131M0x::isResetOK(void) { return (readRegister(CMD_RESET)); }
 
 /**
  * @brief basic initialisation,
- * call '.SetClockSpeed' before to set custom SPI-Clock (default=1MHz),
- * call '.reset' to make extra hardware-reset (optional)
- *
- * @param port      Pointer to SPIClass object
- * @param clk_pin
- * @param miso_pin
- * @param mosi_pin
- * @param cs_pin
- * @param drdy_pin
  */
-void ADS131M0x::begin(SPIClass *port, uint8_t clk_pin, uint8_t miso_pin, uint8_t mosi_pin,
-                      uint8_t cs_pin, uint8_t drdy_pin) {
-	// Set pins up
-	csPin   = cs_pin;
-	drdyPin = drdy_pin;
+void ADS131M0x::init(uint8_t cs_pin, uint8_t drdy_pin, uint8_t reset_pin) {
+	csPin    = cs_pin;
+	drdyPin  = drdy_pin;
+	resetPin = reset_pin;
+}
+
+void ADS131M0x::setupAccess(SPIClass *port, uint32_t spi_clock_speed, uint8_t clk_pin,
+                            uint8_t miso_pin, uint8_t mosi_pin) {
 	spiPort = port;
 
-	spiPort->begin(clk_pin, miso_pin, mosi_pin, cs_pin); // SCLK, MISO, MOSI, SS
-	SPISettings settings(spiClockSpeed, SPI_MSBFIRST, SPI_MODE1);
+	spiPort->begin(clk_pin, miso_pin, mosi_pin, csPin); // SCLK, MISO, MOSI, SS
+	SPISettings settings(spi_clock_speed, SPI_MSBFIRST, SPI_MODE1);
 	spiPort->beginTransaction(settings);
 	delay(1);
 
 	pinMode(csPin, OUTPUT);
 	digitalWrite(csPin, HIGH); // CS HIGH --> not selected
-	pinMode(drdyPin, INPUT);   // DRDY Input
 }
 
 /**
@@ -470,12 +483,7 @@ bool ADS131M0x::setChannelGainCalibration(uint8_t channel, uint32_t gain) {
 
 /// @brief hardware-pin test if data is ready
 /// @return
-bool ADS131M0x::isDataReady() {
-	if (digitalRead(drdyPin) == HIGH) {
-		return false;
-	}
-	return true;
-}
+bool ADS131M0x::isDataReady() { return LOW == digitalRead(drdyPin); }
 
 static inline int32_t readChannelHelper(const uint8_t *buffer, int index, size_t buffer_length) {
 	assert(index >= 0);
@@ -546,33 +554,70 @@ ADS131M0x::AdcRawOutput ADS131M0x::rawReadADC() {
 	return res;
 }
 
-uint16_t ADS131M0x::crc16ccitt(const void *data, size_t count) {
-	const uint8_t *ptr = static_cast<const uint8_t *>(data);
-
-	uint16_t crc = CRC_INIT_VAL;
-	while (count > 0) {
-		--count;
-		static_assert(sizeof(*ptr << 8) >= sizeof(crc));
-		crc ^= *ptr << 8;
-		++ptr;
-		uint8_t i = 8;
-		do {
-			if (crc & 0x8000) {
-				crc <<= 1;
-				crc ^= CRC_POLYNOM;
-			} else {
-				crc <<= 1;
-			}
-		} while (--i);
-	}
-	return crc;
-}
-
 bool ADS131M0x::isCrcOk(const AdcRawOutput *data) {
 	const uint8_t *ptr = reinterpret_cast<const uint8_t *>(&data->crc);
 
 	uint16_t crc           = (ptr[0] << 8) | ptr[1];
-	uint16_t calculatedCrc = ADS131M0x::crc16ccitt(data, sizeof(*data) - DATA_WORD_LENGTH);
+	uint16_t calculatedCrc = crc16ccitt(ptr, sizeof(*data) - DATA_WORD_LENGTH);
 
 	return crc == calculatedCrc;
 }
+
+void ADS131M0x::attachISR(AdcISR isr) {
+	// TODO: figure out if digitalPinToGPIONumber(drdyPin) is needed
+	// see also <io_pin_remap.h>
+	pinMode(drdyPin, INPUT);
+	attachInterruptArg(drdyPin, isr, (void *)1, FALLING);
+	return;
+
+	// gpio_set_direction((gpio_num_t)drdyPin, GPIO_MODE_INPUT);
+	// esp_err_t err = gpio_install_isr_service(0);
+	// if ((err != ESP_OK) && (err != ESP_ERR_INVALID_STATE))
+	// 	return;
+	// gpio_set_intr_type((gpio_num_t)drdyPin, GPIO_INTR_NEGEDGE);
+	// gpio_isr_handler_add((gpio_num_t)drdyPin, isr, nullptr);
+}
+
+#if (CONFIG_MOCK_ADC == 1)
+#include <driver/timer.h>
+
+static bool timerCallback(void *arg) {
+	((ADS131M0x::AdcISR)arg)(nullptr);
+	return true;
+}
+
+void MockAdc::attachISR(AdcISR isr) {
+
+	static constexpr timer_config_t config = {
+	    .alarm_en    = TIMER_ALARM_EN,
+	    .counter_en  = TIMER_PAUSE,
+	    .intr_type   = TIMER_INTR_LEVEL,
+	    .counter_dir = TIMER_COUNT_UP,
+	    .auto_reload = TIMER_AUTORELOAD_EN,
+	    .clk_src     = TIMER_SRC_CLK_DEFAULT,
+	    .divider     = 0x80,
+	};
+
+	constexpr timer_group_t gr  = TIMER_GROUP_0;
+	constexpr timer_idx_t   idx = TIMER_0;
+
+	if (ESP_OK != timer_init(gr, idx, &config))
+		return;
+	timer_set_counter_value(gr, idx, 0);
+	timer_set_alarm_value(gr, idx, 0x4000);
+	timer_isr_callback_add(gr, idx, timerCallback, (void *)isr, 0);
+	timer_enable_intr(gr, idx);
+	timer_start(gr, idx);
+}
+
+auto MockAdc::rawReadADC() -> AdcRawOutput {
+	AdcRawOutput a{
+	    .status = 0x1234,
+	};
+	static uint8_t val = 42;
+	for (int i = 3; i < sizeof(a.data); i += 3) {
+		a.data[i] = ++val;
+	}
+	return a;
+}
+#endif // CONFIG_MOCK_ADC

main/ADS131M0x.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "sdkconfig.h"
+
 class SPIClass;
 
 // define for 2-channel version ADS131M02
@@ -246,12 +248,12 @@ class SPIClass;
  *
  */
 class ADS131M0x {
-  public:
 	static constexpr size_t NUM_CHANNELS_ENABLED = 4;
 	static constexpr size_t DATA_WORD_LENGTH     = 3; // in bytes
 	static constexpr size_t ADC_READ_DATA_SIZE =
 	    (1 + NUM_CHANNELS_ENABLED + 1) * DATA_WORD_LENGTH; // status, channels, CRC
 
+  public:
 	struct AdcOutput {
 		uint16_t status;
 		int32_t  ch0;
@@ -273,11 +275,12 @@ class ADS131M0x {
 #pragma pack(pop)
 	static_assert(sizeof(AdcRawOutput) == ADC_READ_DATA_SIZE);
 
-	void begin(SPIClass *port, uint8_t clk_pin, uint8_t miso_pin, uint8_t mosi_pin, uint8_t cs_pin,
-	           uint8_t drdy_pin);
+	void   init(uint8_t cs_pin, uint8_t drdy_pin, uint8_t reset_pin);
+	void   setupAccess(SPIClass *port, uint32_t spi_clock_speed, uint8_t clk_pin, uint8_t miso_pin,
+	                   uint8_t mosi_pin);
 	int8_t isDataReadySoft(uint8_t channel);
 	bool   isDataReady(void);
-	void   reset(uint8_t reset_pin);
+	void   reset(void);
 	bool   isResetStatus(void);
 	bool   isLockSPI(void);
 	bool   setDrdyFormat(uint8_t drdyFormat);
@@ -294,10 +297,11 @@ class ADS131M0x {
 
 	uint16_t isResetOK(void);
 
+	typedef void (*AdcISR)(void *);
+	void attachISR(AdcISR isr);
+
 	AdcOutput    readADC(void);
 	AdcRawOutput rawReadADC(void);
-
-	void setClockSpeed(uint32_t cspeed);
 
 	uint8_t  writeRegister(uint8_t address, uint16_t value);
 	void     writeRegisterMasked(uint8_t address, uint16_t value, uint16_t mask);
@@ -308,13 +312,40 @@ class ADS131M0x {
   private:
 	uint8_t csPin;
 	uint8_t drdyPin;
+	uint8_t resetPin;
 
 	SPIClass *spiPort;
-	uint32_t  spiClockSpeed = 1000000; // default 1MHz SPI-clock
-
-	static constexpr uint16_t CRC_INIT_VAL = 0xFFFF;
-	static constexpr uint16_t CRC_POLYNOM  = 0x1021;
-
-	static uint16_t crc16ccitt(const void *ptr, size_t count);
 };
+
+#if (CONFIG_MOCK_ADC == 1)
+class MockAdc {
+  public:
+	void init(uint8_t cs_pin, uint8_t drdy_pin, uint8_t reset_pin) {}
+	void setupAccess(SPIClass *port, uint32_t spi_clock_speed, uint8_t clk_pin, uint8_t miso_pin,
+	                 uint8_t mosi_pin) {}
+	void reset() {}
+	bool setChannelPGA(uint8_t channel, uint16_t pga) { return true; }
+	bool setPowerMode(uint8_t powerMode) { return true; }
+	bool setInputChannelSelection(uint8_t channel, uint8_t input) { return true; }
+	bool setOsr(uint16_t osr) { return true; };
+
+	uint16_t readRegister(uint8_t address) { return 0; }
+
+	typedef ADS131M0x::AdcISR       AdcISR;
+	typedef ADS131M0x::AdcRawOutput AdcRawOutput;
+
+	void attachISR(AdcISR isr);
+
+	AdcRawOutput rawReadADC();
+
+	void setClockSpeed(uint32_t cspeed) {}
+
+	static bool isCrcOk(const AdcRawOutput *data) { return true; };
+};
+
+typedef MockAdc AdcClass;
+#else
+typedef ADS131M0x AdcClass;
+#endif
+
 #endif

main/CMakeLists.txt
@@ -1,5 +1,5 @@
 idf_component_register(
-    SRCS "main.cpp" "adc_proc.cpp" "ble_proc.cpp""ADS131M0x.cpp"
+    SRCS "main.cpp" "adc_proc.cpp" "ble_proc.cpp" "ADS131M0x.cpp"
     INCLUDE_DIRS ""
 )
 

main/Kconfig.projbuild
@@ -1,0 +1,9 @@
+menu "Dynamite sampler Firmware configuration"
+
+    config MOCK_ADC
+        bool "Use the mock ADC class"
+        default n
+        help
+            Use this on hardware that doesn't have the ADC.
+
+endmenu

main/adc_proc.cpp
@@ -2,7 +2,6 @@
 
 #include <SPI.h>
 #include <freertos/FreeRTOS.h>
-#include <io_pin_remap.h>
 
 #include "adc_ble_interface.h"
 #include "adc_proc.h"
@@ -10,14 +9,14 @@
 #include "debug_pin.h"
 #include <HardwareSerial.h>
 
-static ADS131M0x    adc;
+static AdcClass     adc;
 static SPIClass     spiADC(HSPI);
 static TaskHandle_t adcReadTaskHandle = NULL;
 
 // When we flag a piece of code with the IRAM_ATTR attribute, the compiled code
 // is placed in the ESP32’s Internal RAM (IRAM). Otherwise the code is kept in
 // Flash. And Flash on ESP32 is much slower than internal RAM.
-static void IRAM_ATTR isrAdcDrdy() {
+static void IRAM_ATTR isrAdcDrdy(void *) {
 
 	// unblock the task that will read the ADC & handle putting in the buffer
 	if (adcReadTaskHandle != NULL) {
@@ -31,7 +30,7 @@ static void IRAM_ATTR isrAdcDrdy() {
 // When accumulated enough, notify the ble task
 static void adcReadAndBuffer() {
 
-	ADS131M0x::AdcRawOutput adcReading = adc.rawReadADC();
+	AdcClass::AdcRawOutput adcReading = adc.rawReadADC();
 
 	if (!bleAccess.deviceConnected)
 		return;
@@ -73,13 +72,13 @@ static void taskSetupAdc(void *setupDone) {
 	pinMode(PIN_DEBUG_TOP, OUTPUT);
 	pinMode(PIN_DEBUG_BOT, OUTPUT);
 
-	adc.setClockSpeed(20000000); // SPI clock speed, has to run before adc.begin()
-	adc.begin(&spiADC, PIN_NUM_CLK, PIN_NUM_MISO, PIN_NUM_MOSI, PIN_CS_ADC, PIN_DRDY);
+	adc.init(PIN_CS_ADC, PIN_DRDY, PIN_ADC_RESET);
+	adc.setupAccess(&spiADC, 20000000, PIN_NUM_CLK, PIN_NUM_MISO, PIN_NUM_MOSI);
 
 	// adc.setMultiplexer(0x00); // AIN0 AIN1
 	// adc.setPGAbypass(0);
 
-	adc.reset(PIN_ADC_RESET);
+	adc.reset();
 
 	for (uint8_t chan = 0; chan < 4; ++chan) {
 		adc.setInputChannelSelection(chan, INPUT_CHANNEL_MUX_DEFAULT_INPUT_PINS);
@@ -93,17 +92,13 @@ static void taskSetupAdc(void *setupDone) {
 
 	adc.setOsr(OSR_4096);
 
-	for (uint8_t addr = 0; addr < 4; ++addr) {
-		uint16_t contents = adc.readRegister(addr);
+	for (int addr = 0; addr < 4; ++addr) {
 		Serial.print(addr);
 		Serial.print(" register contents ");
-		Serial.println(contents);
+		Serial.println(adc.readRegister(addr));
 	}
 
-	// TODO figure out if this function call is needed
-	// The digitalPinToInterrupt() function takes a pin as an argument, and
-	// returns the same pin if it can be used as an interrupt.
-	attachInterrupt(digitalPinToGPIONumber(PIN_DRDY), isrAdcDrdy, FALLING);
+	adc.attachISR(isrAdcDrdy);
 
 	// TODO figure out if you need to setup wake from sleep for gpio
 
@@ -115,7 +110,7 @@ void setupAdc(int core) {
 	volatile bool done = false;
 	xTaskCreatePinnedToCore(taskSetupAdc, "task_ADC_setup", 1024 * 5, (void *)&done, 1, NULL, core);
 	while (!done)
-		;
+		vTaskDelay(10);
 
 	// TODO figure out the memory stack required
 	const UBaseType_t priority = 24; // Highest priority possible

main/main.cpp
@@ -54,6 +54,14 @@ extern "C" void app_main(void) {
 	Serial.println(GIT_REVISION);
 	Serial.print("Running on Core: ");
 	Serial.println(xPortGetCoreID());
+
+#ifndef CONFIG_MOCK_ADC
+	Serial.println("Mock adc flag not set");
+#else
+	Serial.print("Mock ADC flag: ");
+	Serial.println(CONFIG_MOCK_ADC);
+#endif
+
 	// Serial.print("Config PM SLP IRAM OPT (put lightsleep into ram):");
 	// Serial.println(CONFIG_PM_SLP_IRAM_OPT);
 

sdkconfig.common
@@ -20,9 +20,11 @@ CONFIG_ARDUINO_SELECTIVE_SimpleBLE=n
 CONFIG_ARDUINO_SELECTIVE_RainMaker=n
 CONFIG_ARDUINO_SELECTIVE_OpenThread=n
 CONFIG_ARDUINO_SELECTIVE_Insights=n
+
 CONFIG_USE_AFE=n
 CONFIG_USE_WAKENET=n
 CONFIG_USE_MULTINET=n
+
 CONFIG_BT_ENABLED=y
 CONFIG_BT_NIMBLE_ENABLED=y
 CONFIG_BT_NIMBLE_MAX_CONNECTIONS=1
@@ -46,3 +48,9 @@ CONFIG_PARTITION_TABLE_TWO_OTA=n
 CONFIG_ESPTOOLPY_FLASHSIZE_4MB=y
 CONFIG_ESPTOOLPY_FLASHSIZE="4MB"
 CONFIG_ESPTOOLPY_HEADER_FLASHSIZE_UPDATE=y
+
+CONFIG_COMPILER_WARN_WRITE_STRINGS=y
+CONFIG_COMPILER_HIDE_PATHS_MACROS=y
+CONFIG_COMPILER_CXX_EXCEPTIONS=n
+CONFIG_COMPILER_CXX_RTTI=n
+CONFIG_COMPILER_ORPHAN_SECTIONS_WARNING=y

sdkconfig.debug
@@ -5,6 +5,7 @@ CONFIG_BT_NIMBLE_LOG_LEVEL_WARNING=y
 # CONFIG_BT_NIMBLE_DEBUG
 # CONFIG_COMPILER_STACK_CHECK_MODE_NORM
 # CONFIG_COMPILER_STACK_CHECK_MODE_STRONG
-# CONFIG_COMPILER_STACK_CHECK_MODE_ALL
+CONFIG_COMPILER_STACK_CHECK_MODE_ALL=y
 # CONFIG_FREERTOS_CHECK_STACKOVERFLOW_NONE
 # CONFIG_FREERTOS_CHECK_STACKOVERFLOW_PTRVAL
+CONFIG_FREERTOS_CHECK_STACKOVERFLOW_CANARY=y

commit a3ac58a38f4c12c47d98ce992f312d63586d1686

Fix CRC error introduced by #31

main/ADS131M0x.cpp
@@ -194,10 +194,11 @@ auto ADS131M0x::rawReadADC() -> const AdcRawOutput * {
 }
 
 bool ADS131M0x::isCrcOk(const AdcRawOutput *data) {
-	const uint8_t *ptr = reinterpret_cast<const uint8_t *>(&data->crc);
+	const uint8_t *crc_ptr = reinterpret_cast<const uint8_t *>(&data->crc);
+	uint16_t       crc     = (crc_ptr[0] << 8) | crc_ptr[1];
 
-	uint16_t crc           = (ptr[0] << 8) | ptr[1];
-	uint16_t calculatedCrc = crc16ccitt(ptr, sizeof(*data) - DATA_WORD_LENGTH);
+	const uint8_t *data_cast     = reinterpret_cast<const uint8_t *>(data);
+	uint16_t       calculatedCrc = crc16ccitt(data_cast, sizeof(*data) - DATA_WORD_LENGTH);
 
 	if (crc != calculatedCrc) {
 		ESP_LOGE(TAG, "CRC err %X != %X", crc, calculatedCrc);