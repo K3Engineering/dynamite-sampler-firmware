1301e2dc95151f241a8b0abac4d51635350f9bab
Larry <--global>
2025-02-08 17:30:28 -0500

    Adding comments about UUIDS

--- main/dynamite_sampler_api.h
+++ main/dynamite_sampler_api.h
@@ -5,10 +5,15 @@
 
 //======================== <UUIDs>
 
+// Bluetooth assigned UUIDs
+// https://www.bluetooth.com/wp-content/uploads/Files/Specification/Assigned_Numbers.html
 constexpr uint16_t DEVICE_INFO_SVC_UUID         = 0x180A;
 constexpr uint16_t DEVICE_MAKE_NAME_CHR_UUID    = 0x2A29;
 constexpr uint16_t DEVICE_FIRMWARE_VER_CHR_UUID = 0x2A26;
 
+// Dynamite sampler specific UUIDs
+// TODO: the ADC_FEED_CHR_UUID & OTA UUIDs were copied from sample code originally.
+// They should be changed to be unique.
 constexpr char DYNAMITE_SAMPLER_SVC_UUID[] = "e331016b-6618-4f8f-8997-1a2c7c9e5fa3";
 constexpr char ADC_FEED_CHR_UUID[]         = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 constexpr char LC_CALIB_CHR_UUID[]         = "10adce11-68a6-450b-9810-ca11b39fd283";
