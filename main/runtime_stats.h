a4bcf4976a0c5e221d6765c5bb64fc5e59e4877a
dl-dl <dl.toronto@outlook.com>
2026-03-07 13:34:47 -0400

    Comment

--- main/runtime_stats.h
+++ main/runtime_stats.h
@@ -3,9 +3,9 @@
 
 // To enable profiling of tasks,
 // use config menu or add this options to the SDKCONFIG file
-// CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS = y
+// FREERTOS_USE_TRACE_FACILITY=y
+// CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS=y
 // Also you need log trace level >= INFO to see the output
-
 void setupStats(int core);
 
 #endif // _RUNTIME_STATS_H
