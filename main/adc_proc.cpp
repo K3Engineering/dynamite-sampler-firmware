edf421a8f04e7ef63eebe4ad1f967608fc55a772
Vlad K <2459295+vladkvit@users.noreply.github.com>
2026-07-28 19:23:40 -0400

    format

--- main/adc_proc.cpp
+++ main/adc_proc.cpp
@@ -136,11 +136,10 @@ static void configureAdc() {
 
 static void taskSetupAdc(void *setupDone) {
 	// adc_proc.cpp — board_cfg.h already included here
-	ESP_LOGI(TAG, "Board %s, ADC[CS=%d DRDY=%d RESET=%d CLK=%d MISO=%d MOSI=%d]",
-			boardConfig.name,
-			boardConfig.adc.hwConnect.cs, boardConfig.adc.hwConnect.drdy,
-			boardConfig.adc.hwConnect.reset, boardConfig.adc.spiConnect.clock,
-			boardConfig.adc.spiConnect.miso, boardConfig.adc.spiConnect.mosi);
+	ESP_LOGI(TAG, "Board %s, ADC[CS=%d DRDY=%d RESET=%d CLK=%d MISO=%d MOSI=%d]", boardConfig.name,
+	         boardConfig.adc.hwConnect.cs, boardConfig.adc.hwConnect.drdy,
+	         boardConfig.adc.hwConnect.reset, boardConfig.adc.spiConnect.clock,
+	         boardConfig.adc.spiConnect.miso, boardConfig.adc.spiConnect.mosi);
 
 	ESP_LOGI(TAG, "setting up adc on core: %u", esp_cpu_get_core_id());
 
